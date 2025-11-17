a58e4630f56ebd9fab740dd0bd6dd9608006bdd7
vally68 <david.balga@gmail.com>
2025-11-17 16:51:40 +0100

    feat: début du "défilement" qui va  récupérer les derniers messages Style: css mise en forme pour intégrer le défilement à venir

--- view/home.php
+++ view/home.php
@@ -2,12 +2,30 @@
   
   </header>
 <section class="home-welcome">
-  <div class="home-content">
-    <h1>BIENVENUE SUR LE FORUM</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
-    <div class="home-actions">
-      <a class="btn btn-primary" href="index.php?ctrl=security&action=login">Se connecter</a>
-      <a class="btn btn-secondary" href="index.php?ctrl=security&action=register">S'inscrire</a>
+    
+    <svg class="background-line" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 1080" preserveAspectRatio="none">
+        <path
+            d="M22.5 1057.5 
+               L22.5 757.5 
+               L1800.5 757.5 
+               L1800.5 357.5 
+               L22.5 357.5 
+               L22.5 57.5"
+            fill="none"
+            stroke="#F9F4DA"
+            stroke-width="100"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+    </svg>
+
+    <div class="home-content">
+        <h1>BIENVENUE SUR LE FORUM</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+        <div class="home-actions">
+            <a class="btn btn-primary" href="#">Se connecter</a>
+            <a class="btn btn-secondary" href="#">S'inscrire</a>
+        </div>
     </div>
-  </div>
+
 </section>
