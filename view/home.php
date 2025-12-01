4f5b0290b13fc940908a5524ab73858fa2a896a0
Vally68 <david.balga@gmail.com>
2025-12-01 15:17:23 +0100

    fix correction

--- view/home.php
+++ view/home.php
@@ -33,7 +33,7 @@ $lastMessages = isset($result["data"]["lastMessages"]) ? $result["data"]["lastMe
 
   <div class="home-content">
     <h1>BIENVENUE SUR LE FORUM</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+    <p>WELCOME TO THE JUNGLE</p>
     <div class="home-actions">
              <div id="nav-right">
             <?php if (App\Session::getUser()): ?>
